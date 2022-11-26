852b071abb4c2d2c4eae491c8a4a2434b5983c8a
cedMac <c.mace@webandcow.com>
2022-11-26 13:51:57 +0100

    Ajout de selection des carrés

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-new Vue({
-    el: '#app',
-    data: {
-        topLeft: false,
-        topRight: false,
-        bottomLeft: false,
-        bottomRight: false,
-        sequence: [],
-        squareMapping : ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'],
-        },
-    methods: {
-        addNewElemToSequence(){
-            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
-        },
+const vm = new Vue({
+  el: "#app",
+  data: {
+    topLeft: false,
+    topRight: false,
+    bottomLeft: false,
+    bottomRight: false,
+    sequence: [],
+    squareMapping: ["topLeft", "topRight", "bottomLeft", "bottomRight"],
+  },
+  methods: {
+    addNewElemToSequence() {
+      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+    },
 
-        newgame() {
-            this.sequence = [];
-            this.addNewElemToSequence();
-        }
+    allgray() {
+      this.topLeft = false;
+      this.topRight = false;
+      this.bottomLeft = false;
+      this.bottomRight = false;
+    },
+
+    newgame() {
+      this.sequence = [];
+      this.addNewElemToSequence();
+      this[this.sequence[0]] = true;
+      setTimeout(function () {
+        vm.allgray();
+      }, 300);
+    },
+
+    selectSquare(square) {
+        console.log(square);
     }
-})
+  },
+});
