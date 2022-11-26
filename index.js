789261c34e9cbec3f90a512eeb3e0879ba7a50cc
cedMac <c.mace@webandcow.com>
2022-11-26 13:38:54 +0100

    ajout date vue

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 new Vue({
     el: '#app',
+    data: {
+        topLeft: false,
+        topRight: false,
+        bottomLeft: false,
+        bottomRight: false,
+        sequence: [],
+        squareMapping : ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'],
+        },
+    methods: {
+        addNewElemToSequence(){
+            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+        },
+
+        newgame() {
+            this.sequence = [];
+            this.addNewElemToSequence();
+        }
+    }
 })
