ecb2e659063878179df7e843e666aeb99e0b7ac1
cedMac <c.mace@webandcow.com>
2022-11-26 14:26:20 +0100

    Ajout des tour sde jeu et selection du joueur

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const vm = new Vue({
     bottomLeft: false,
     bottomRight: false,
     sequence: [],
+    tmp: [],
     squareMapping: ["topLeft", "topRight", "bottomLeft", "bottomRight"],
   },
   methods: {
     addNewElemToSequence() {
       this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+      this.tmp = this.sequence.slice();
     },
 
     allgray() {
@@ -22,15 +24,49 @@ const vm = new Vue({
 
     newgame() {
       this.sequence = [];
+      this.nextTurn();
+      //   this[this.sequence[0]] = true;
+      //   this.tmp = this.sequence.slice();
+
+      //   setTimeout(function () {
+      //     vm.allgray();
+      //   }, 300);
+    },
+
+    nextTurn() {
       this.addNewElemToSequence();
-      this[this.sequence[0]] = true;
+      this.allgray();
+      this.playSequence(this.tmp[0]);
+    },
+
+    playSequence(square) {
+      this[square] = true;
       setTimeout(function () {
         vm.allgray();
-      }, 300);
+        vm.tmp.shift();
+        if (vm.tmp[0]) {
+          setTimeout(function () {
+            vm.playSequence(vm.tmp[0]);
+          }, 400);
+        } else {
+          vm.tmp = vm.sequence.slice();
+        }
+      }, 400);
     },
 
     selectSquare(square) {
-        console.log(square);
-    }
+      if (square === this.tmp[0]) {
+        this[square] = true;
+        setTimeout(function () {
+          vm.allgray();
+          vm.tmp.shift();
+          if (!vm.tmp[0]) {
+            vm.nextTurn();
+          } 
+        }, 400);
+      } else {
+        alert("LOST");
+      }
+    },
   },
 });
